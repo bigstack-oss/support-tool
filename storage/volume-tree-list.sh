2558a6a437405372db97bbabf0a96d1859e90fb0
Roy Tan <yooniks9@gmail.com>
2025-09-27 23:18:21 +0800

    change info layout

--- storage/volume-tree-list.sh
+++ storage/volume-tree-list.sh
@@ -63,7 +63,7 @@ if [ "$status" == "in-use" ]; then
     volume_details=$(openstack volume show $vid -f json)
     server_id=$(echo $volume_details | jq -r '.attachments[0].server_id')
     server_hostname=$(openstack server show $server_id -c hostname -f value 2>/dev/null)
-    echo "$main_volume ($status) - server: $server_hostname($server_id)"
+    echo "$main_volume ($status) - server_id: $server_id($server_hostname)"
 else
     echo "$main_volume ($status)"
 fi
